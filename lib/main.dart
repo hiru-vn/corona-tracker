05f16a5d2d1f423a6607b1060f91edba93149d42
Nguyễn Quang Huy <quanghuy1998kh@gmail.com>
2020-06-11 21:29:27 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:corona_tracker/corona_tracker.dart';
+import 'package:corona_tracker/json/address.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // List add = AddressVn.getJsonData();
+  // for(int i=0; i< add.length; i++) {
+  //   print("INSERT into \"CITY\"(id, name) VALUES (${add[i]["pid"]}, '${add[i]["pn"]}')");
+  // }
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     runApp(CoronaTracker());
   });
 }
+
+
+
