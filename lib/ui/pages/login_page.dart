94081a9f1bb67b10569dea2056b8f13de3aacdf0
EdwardLauv <43104916+edwardphuc@users.noreply.github.com>
2020-06-12 18:31:44 +0700

    done sign in, sign up

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,9 +1,13 @@
+import 'package:corona_tracker/providers/home/home_controller.dart';
+import 'package:corona_tracker/services/navigate_services.dart';
+import 'package:corona_tracker/ui/pages/home_page.dart';
 import 'package:corona_tracker/ui/pages/register_page.dart';
+import 'package:corona_tracker/ui/widgets/info/notification.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-
-
-
+import 'package:provider/provider.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -11,9 +15,13 @@ class LoginPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<LoginPage> {
-
   TextEditingController _emailController = new TextEditingController();
   TextEditingController _passController = new TextEditingController();
+  bool checkSingIn = false;
+  var _usernameErr = "Tài khoản không hợp lệ";
+  var _passwordErr = "Mật khẩu không hợp lệ";
+  bool userinvalid = false;
+  bool passinvalid = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,16 +35,20 @@ class _RegisterPageState extends State<LoginPage> {
               SizedBox(
                 height: 60,
               ),
-              Image.asset('assets/images/logo.png', width: 150, height: 150, fit: BoxFit.fill),
+              Image.asset('assets/images/logo.png',
+                  width: 150, height: 150, fit: BoxFit.fill),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                      "Chào mừng bạn \nĐến với Corona-tracker!",
-                      style: TextStyle(fontSize:25, color: Color(0xff333333),fontWeight: FontWeight.bold),
-                    ),
+                  "Chào mừng bạn Đến với\nCorona-tracker!",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Color(0xff333333),
+                      fontWeight: FontWeight.bold),
+                ),
               ),
               Padding(
-                padding: const EdgeInsets.fromLTRB(0,30,0,0),
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
                 child: Text(
                   "Đăng nhập để sử dụng tính năng",
                   style: TextStyle(fontSize: 16, color: Colors.blue),
@@ -51,12 +63,15 @@ class _RegisterPageState extends State<LoginPage> {
                           decoration: InputDecoration(
                               labelText: "Email",
                               errorText:
-                                  snapshot.hasError ? snapshot.error : null,
-                              prefixIcon: Container(
-                                  width: 50, child: Image.asset("")),
+                                  userinvalid ? _usernameErr : null,
+                              prefixIcon:
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(width: 30, child: Image.asset("assets/images/gmail.png")),
+                                  ),
                               border: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.black, width: 1),
+                                  borderSide:
+                                      BorderSide(color: Colors.black, width: 1),
                                   borderRadius:
                                       BorderRadius.all(Radius.circular(6)))),
                         )),
@@ -68,13 +83,13 @@ class _RegisterPageState extends State<LoginPage> {
                         style: TextStyle(fontSize: 18, color: Colors.black),
                         decoration: InputDecoration(
                             errorText:
-                                snapshot.hasError ? snapshot.error : null,
-                            labelText: "Password",
-                            prefixIcon: Container(
-                                width: 50, child: Image.asset("")),
+                                passinvalid ? _passwordErr : null,
+                            labelText: "Mật khẩu",
+                            prefixIcon:
+                                Container(width: 50, child: Image.asset("assets/images/key.png")),
                             border: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Colors.black,width: 1),
+                                borderSide:
+                                    BorderSide(color: Colors.black, width: 1),
                                 borderRadius:
                                     BorderRadius.all(Radius.circular(6)))),
                       )),
@@ -89,6 +104,7 @@ class _RegisterPageState extends State<LoginPage> {
                       "Đăng nhập",
                       style: TextStyle(color: Colors.white, fontSize: 18),
                     ),
+                  
                     color: Color(0xff3277D8),
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.all(Radius.circular(6))),
@@ -100,15 +116,19 @@ class _RegisterPageState extends State<LoginPage> {
                 child: RichText(
                   text: TextSpan(
                       text: "Bạn chưa có tài khoản? ",
-                      style: TextStyle(color: Colors.black,fontSize: 16),
+                      style: TextStyle(color: Colors.black, fontSize: 16),
                       children: <TextSpan>[
                         TextSpan(
                             text: "Đăng kí",
                             style: TextStyle(
                                 color: Color(0xff3277D8), fontSize: 16),
-                            recognizer: new TapGestureRecognizer()..onTap=()=>{
-                              Navigator.push(context, MaterialPageRoute(builder: (context){ return RegisterPage();}))
-                            }),
+                            recognizer: new TapGestureRecognizer()
+                              ..onTap = () => {
+                                    Navigator.push(context,
+                                        MaterialPageRoute(builder: (context) {
+                                      return RegisterPage();
+                                    }))
+                                  }),
                       ]),
                 ),
               )
@@ -138,31 +158,78 @@ class _RegisterPageState extends State<LoginPage> {
   //   }
   // }
 
-void clickSignin()
-{
-  setState((){
-    
-    });
-}
+  void clickSignin() {
+    setState(() {
+      Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+      RegExp regex = new RegExp(pattern);
+      if(!regex.hasMatch(_emailController.text.toString())|| _emailController.text.length<6)
+      {
+         userinvalid=true;
+      }
+      else userinvalid =false;
+      if(_passController.text.length<6)
+      {
+        passinvalid=true;
+      }
+      else passinvalid=false;
 
+      if(!userinvalid && !passinvalid)
+      {
+        signin();
+        if (checkSingIn == true) {
+        Alert(
+          context: context,
+          type: AlertType.success,
+          title: "Đăng nhập thành công",
+          desc: "bây giờ bạn có thể sử dụng các tính năng",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "Xong",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ChangeNotifierProvider(
+                          create: (_) => HomeController(), child: HomePage()))),
+              width: 120,
+            )
+          ],
+        ).show();
+        ;
+        ;
+      } else {
+        Alert(
+          context: context,
+          type: AlertType.error,
+          title: "Đăng nhập thất bại",
+          desc: "Vui lòng kiểm tra lại Email hoặc mật khẩu",
+        ).show();
+        ;
+        ;
+      }
+        }
+      
+      });
+  }
 
+  void signin() {
+    loginapi(_emailController.text, _passController.text);
+  }
 
-  // void clicksignup()
-  // {
-  //   loginapi(_nameController.text,_phoneController.text,_emailController.text,_passController.text);
-  // }
-  // void loginapi(String name, String phonenumber, String email, String password) async {
-  //   print(name);
-  //   var dio = Dio();
-  //   Response response;
-  //   const  baseURL = "http://127.0.0.1:3000/user/sign-up";
-  //   var data = {
-  //     "fullName": "sadsadsadassdawqfsa",
-	//     "username": "hahasad.com",
-	//     "password": "password123123"
-    
-  //   };
-  //   response = await dio.post(baseURL,data: data)  ;
-  //   print(response.data.toString());
-  // }
+  void loginapi(String username, String password) async {
+    var dio = Dio();
+    Response response;
+    const baseURL = "http://127.0.0.1:3000/user/sign-in";
+    var data = {"username": username, "password": password};
+    response = await dio.post(baseURL, data: data);
+    print(response.data.toString());
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      print(response.statusCode);
+      checkSingIn = true;
+    }
+  }
 }
--- lib/ui/pages/register_page.dart
+++ lib/ui/pages/register_page.dart
@@ -1,7 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:corona_tracker/json/address.dart';
+import 'package:intl/intl.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -11,12 +13,22 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   TextEditingController _nameController = new TextEditingController();
   TextEditingController _emailController = new TextEditingController();
+  var _emailErr ='Email đăng kí không hợp lệ';
+  bool emailinvalid = false;
   TextEditingController _passController = new TextEditingController();
+  var _passwordErr ='Mật khẩu phải dài hơn 6 kí tự';
+  bool passinvalid = false;
+  TextEditingController _confirmpassController = new TextEditingController();
+  var _confimErr = 'Xác nhận mật khẩu sai';
+  bool confiminvalid = false;
   TextEditingController _phoneController = new TextEditingController();
+  var _phoneErr ='Số điện thoại phải 10 số';
+  bool phoneinvalid =false;
+  TextEditingController _tinhthanhphoController = new TextEditingController();
+  TextEditingController _tamtruController = new TextEditingController();
+  DateTime _dateTime;
   final List<Map> _addressVn = AddressVn.getJsonData();
-  String _mySelection, _mySelectionWn, _mySelectionDn;
-  bool choosedn = false;
-  bool choosepn = false;
+  String _mySelection;
 
   get parsedJson => null;
   @override
@@ -35,7 +47,7 @@ class _RegisterPageState extends State<RegisterPage> {
               child: Column(
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 40, 0, 0),
+                    padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
                     child: Text(
                       "Chào mừng bạn đến với Corona-tracker",
                       style: TextStyle(
@@ -57,6 +69,7 @@ class _RegisterPageState extends State<RegisterPage> {
                           Padding(
                             padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
                             child: TextField(
+                              controller: _nameController,
                               style:
                                   TextStyle(fontSize: 18, color: Colors.black),
                               decoration: InputDecoration(
@@ -73,10 +86,12 @@ class _RegisterPageState extends State<RegisterPage> {
                           Padding(
                             padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
                             child: TextField(
+                              controller: _emailController,
                               style:
                                   TextStyle(fontSize: 18, color: Colors.black),
                               decoration: InputDecoration(
                                   labelText: "Email",
+                                  errorText: emailinvalid ? _emailErr : null,
                                   labelStyle: TextStyle(
                                       fontSize: 15, color: Colors.black),
                                   border: OutlineInputBorder(
@@ -89,10 +104,12 @@ class _RegisterPageState extends State<RegisterPage> {
                           Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: TextField(
+                              controller: _phoneController,
                               style:
                                   TextStyle(fontSize: 18, color: Colors.black),
                               decoration: InputDecoration(
                                   labelText: "Số điện thoai",
+                                  errorText: phoneinvalid? _phoneErr : null,
                                   labelStyle: TextStyle(
                                       fontSize: 15, color: Colors.black),
                                   border: OutlineInputBorder(
@@ -105,10 +122,32 @@ class _RegisterPageState extends State<RegisterPage> {
                           Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: TextField(
+                              obscureText: true,
+                              controller: _passController,
                               style:
                                   TextStyle(fontSize: 18, color: Colors.black),
                               decoration: InputDecoration(
                                   labelText: "Mật khẩu",
+                                  errorText: passinvalid?_passwordErr:null,
+                                  labelStyle: TextStyle(
+                                      fontSize: 15, color: Colors.black),
+                                  border: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.black, width: 1),
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(6)))),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextField(
+                              obscureText: true,
+                              controller: _confirmpassController,
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.black),
+                              decoration: InputDecoration(
+                                  labelText: "Xác nhận mật khẩu",
+                                  errorText: confiminvalid?_confimErr:null,
                                   labelStyle: TextStyle(
                                       fontSize: 15, color: Colors.black),
                                   border: OutlineInputBorder(
@@ -135,9 +174,11 @@ class _RegisterPageState extends State<RegisterPage> {
                                 children: <Widget>[
                                   const SizedBox(width: 10),
                                   Expanded(
-                                    child: DropdownButton(
+                                      child: DropdownButton(
                                     isExpanded: true,
-                                    hint: _mySelection==null ?  Text("Chọn tỉnh/thành phố") : Text(_mySelection),
+                                    hint: _mySelection == null
+                                        ? Text("Chọn tỉnh/thành phố")
+                                        : Text(_mySelection),
                                     isDense: true,
                                     items: _addressVn.map((Map map) {
                                       return new DropdownMenuItem<String>(
@@ -145,82 +186,65 @@ class _RegisterPageState extends State<RegisterPage> {
                                         child: Text(map["pn"]),
                                       );
                                     }).toList(),
-                                    onChanged: (String newValue)
-                                    {
+                                    onChanged: (String newValue) {
                                       onChange(newValue);
                                     },
                                     underline: const SizedBox(),
-                                  )
-                                )
+                                  ))
                                 ],
                               ),
                             ),
                           ),
                           Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              height: 60,
-                              width: 350,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(6),
-                                border: Border.all(
-                                  color: Colors.grey,
-                                  width: 1,
-                                ),
-                              ),
-                              child: Row(
-                                children: <Widget>[
-                                  const SizedBox(width: 10),
-                                  Expanded( 
-                                    child: DropdownButton(
-                                    isExpanded: true,
-                                    hint: new Text("Chọn quận huyện"),
-                                    isDense: true,
-                                    value: _mySelection,
-                                    items: null,
-                                    onChanged: null,
-                                    underline: const SizedBox(),
-                                  )
-                                )
-                                ],
-                              ),
+                            child: TextField(
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.black),
+                              decoration: InputDecoration(
+                                  labelText: "Nhập địa chỉ tạm trú của bạn",
+                                  labelStyle: TextStyle(
+                                      fontSize: 15, color: Colors.black),
+                                  border: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.black, width: 1),
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(6)))),
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              height: 60,
-                              width: 350,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(6),
-                                border: Border.all(
-                                  color: Colors.grey,
-                                  width: 1,
-                                ),
-                              ),
-                              child: Row(
-                                children: <Widget>[
-                                  const SizedBox(width: 10),
-                                  Expanded(
-                                    child: DropdownButton<String>(
-                                      isExpanded: true,
-                                      hint: new Text("Chọn xã, thị trấn"),
-                                      items: _addressVn.map((Map map) {
-                                        return new DropdownMenuItem<String>(
-                                          value: map.toString(),
-                                          child: Text(map["pn"].toString()),
-                                        );
-                                      }).toList(),
-                                      onChanged: null,
-                                      underline: const SizedBox(),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
+                          Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 300,
+                          height: 52,
+                          child: RaisedButton(
+                            child: Text(( _dateTime==null)?'Chọn ngày sinh': "Ngày sinh:" +_dateTime.day.toString() +"/"+
+                            _dateTime.month.toString() +"/"+ _dateTime.year.toString(),
+                            style: TextStyle(color: Colors.white),),
+                            onPressed: () {
+                              showDatePicker(
+                                      context: context,
+                                      initialDate: _dateTime == null
+                                          ? DateTime.now()
+                                          : _dateTime,
+                                      firstDate: DateTime(1980),
+                                      lastDate: DateTime(2021))
+                                  .then((date) {
+                                setState(() {
+                                  _dateTime = date;
+                                });
+                              });
+                            },
+                            color: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(6))),
                           ),
+                        ),
+                      )
+                    ],
+                  ),
                         ],
                       ),
                     ),
@@ -231,7 +255,7 @@ class _RegisterPageState extends State<RegisterPage> {
                       width: 250,
                       height: 52,
                       child: RaisedButton(
-                        onPressed: clickSignup,
+                        onPressed: checkregister,
                         child: Text(
                           "Đăng kí",
                           style: TextStyle(color: Colors.white, fontSize: 18),
@@ -241,31 +265,48 @@ class _RegisterPageState extends State<RegisterPage> {
                             borderRadius: BorderRadius.all(Radius.circular(6))),
                       ),
                     ),
-                  ),
+                  ),                 
                 ],
               ),
             )));
   }
-  void onChange(String mySelection)
-  {
+
+  void onChange(String mySelection) {
     setState(() {
       _mySelection = mySelection;
-      choosepn = true;
     });
   }
 
-  void onChangews(String _mySelectionWs)
-  {
-    setState(()
-    {
-        if(_mySelection==null || _mySelection =="Chọn tỉnh/thành phố")
-        {
-          
-        }
+  void checkregister() {
+    setState(() {
+      Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+      RegExp regex = new RegExp(pattern);
+      if(!regex.hasMatch(_emailController.text.toString()) || _emailController.text.length<6)
+      {
+        emailinvalid = true;
+      }
+      else emailinvalid = false;
+
+      if(_passController.text.length<6)
+      passinvalid = true;
+      else passinvalid = false;
+
+      if(_confirmpassController.text.toString()!=_passController.text.toString())
+      confiminvalid=true;
+      else confiminvalid = false;
+      if(_phoneController.text.length!=10)
+      phoneinvalid = true;
+      else phoneinvalid = false;
     });
   }
-  void clickSignup()
-  {
-    
+
+  void clickSignupForUser() {
+    var dio = Dio();
+    Response response;
+    String baseURL = "http://127.0.0.1:3000/user/sign-up";
+    var data = {};
   }
+
+  void updateForUser() {}
 }
