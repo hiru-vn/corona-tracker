18de62ba7abbe210185bed749cc9b5bfbc21819b
EdwardLauv <43104916+edwardphuc@users.noreply.github.com>
2020-06-22 16:44:32 +0700

    variable globals

--- lib/globals.dart
+++ lib/globals.dart
@@ -6,8 +6,5 @@ library Users;
   int phone;
 	int cityCode;
   String address;
-<<<<<<< HEAD
   String baseURL = "http://10.45.163.97:3000";
-=======
-  const baseLocalHost = 'http://10.42.0.1:3000/';
->>>>>>> eb346768d7d29b3312cb31dc41c5ec354e9016ab
+
